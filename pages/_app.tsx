4bf012ca98a74ccc356fa1b30410e51c145906b7
Fikri Karim <fkfikrikarim@gmail.com>
2022-08-24 22:58:55 +0700

    Add header, footer and some styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
+import type { AppProps } from "next/app";
+
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col gap-4 py-6 px-4">
+        <h1 className="text-3xl font-bold">Fikri Karim</h1>
+
+        <Component {...pageProps} />
+
+        <div className="flex gap-1.5">
+          <a href="mailto:hello@fikrikarim.com" target="_top">
+            Email
+          </a>
+          &bull;
+          <a href="https://github.com/fikrikarim" rel="noopener noreferrer">
+            Github
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MyApp;
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="bg-gray-100">Test</div>;
+  return <div className="">Test</div>;
 }
